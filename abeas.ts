58a3d4c97f216d947bc2ff2496f378d94104b9b6
sarker1999 <shouvojit.sarker.shouvojit@gmail.com>
2020-01-25 23:29:37 +1100

    returning stack trace of entries implemented

--- abeas.ts
+++ abeas.ts
@@ -2,18 +2,18 @@
 
 import * as fs from 'fs';
 
-function getEarnings(entries: string[][]): number{
+function getEarnings(entries: string[][]): string[]{
     
     const earnings = entries.map(function(value: string[]){
         const eligibleEntries = entries.slice(1).filter(entry => +entry[1] > (+value[1] + +value[2]));
         
-        const totalMoney = (typeof eligibleEntries !== 'undefined' && eligibleEntries.length > 0) ? [getEarnings(eligibleEntries), 1] : [+value[3], 0]
+        const totalMoney: any = (typeof eligibleEntries !== 'undefined' && eligibleEntries.length > 0) ? [getEarnings(eligibleEntries)[0], 1, getEarnings(eligibleEntries)[1].concat(value[0])] : [+value[3], 0, [value[0]]]
         //console.log(eligibleEntries);
         //console.log(totalMoney);
-        return !totalMoney[1] ? +value[3] : +value[3] + totalMoney[0];
+        return !totalMoney[1] ? [+value[3], [value[0]]] : [+value[3] + +totalMoney[0], totalMoney[2]];
     });
     console.log(earnings);
-    return earnings.sort(function(a, b){return b-a})[0];
+    return earnings.sort(function(a, b){return b[0]-a[0]})[0];
 }
 
 const entries = fs.readFileSync('testCases.txt','utf8').split("\n").map(function(row){return row.split(" ");});
